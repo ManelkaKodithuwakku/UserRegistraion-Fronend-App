b8e40012327399a95a700526130152b9b197f4d0
manelka <nishanmanelka@gmail.com>
2024-02-02 13:56:32 +0530

    add login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
-  return (
-    <div className="App">Hello World</div>
-  );
+	const user = localStorage.getItem("token");
+
+	return (
+		<Routes>
+			{user && <Route path="/" exact element={<Main />} />}
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
+	);
 }
 
 export default App;
